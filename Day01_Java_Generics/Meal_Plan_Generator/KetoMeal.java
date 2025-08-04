4bc70b163df3631b0db2c7a18249e83c690ab9d3
aarshabh8171 <saubharia@gmail.com>
2025-08-04 11:27:24 +0530

    [Aarshabh Saubhari] ADD. Meal_Plan_Generator --> KetoMeal

---
+++ Day01_Java_Generics/Meal_Plan_Generator/KetoMeal.java
@@ -0,0 +1,8 @@
+package Day01_Java_Generics.Meal_Plan_Generator;
+
+public class KetoMeal implements MealPlan {
+    @Override
+    public String prepare() {
+        return "Preparing High-Protein Meal...";
+    }
+}
