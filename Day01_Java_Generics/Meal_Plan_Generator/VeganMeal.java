a598dfef3011c43a2ea5e92b2e27a5d251ab10d2
aarshabh8171 <saubharia@gmail.com>
2025-08-04 11:28:42 +0530

    [Aarshabh Saubhari] ADD. Meal_Plan_Generator --> VeganMeal

---
+++ Day01_Java_Generics/Meal_Plan_Generator/VeganMeal.java
@@ -0,0 +1,8 @@
+package Day01_Java_Generics.Meal_Plan_Generator;
+
+public class VeganMeal implements MealPlan {
+    @Override
+    public String prepare() {
+        return "Preparing Vegan Meal...";
+    }
+}
