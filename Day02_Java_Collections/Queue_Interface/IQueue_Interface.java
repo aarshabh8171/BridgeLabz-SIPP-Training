f711dc941e77ab80009cd6bf826b769c6c5ec8c8
aarshabh8171 <saubharia@gmail.com>
2025-08-04 12:00:42 +0530

    [Aarshabh Saubhari] ADD. Queue_Interface --> IQueue_Interface

---
+++ Day02_Java_Collections/Queue_Interface/IQueue_Interface.java
@@ -0,0 +1,13 @@
+package Day02_Java_Collections.Queue_Interface;
+
+import java.util.List;
+import java.util.Queue;
+
+public interface IQueue_Interface {
+    <T> void reverseQueue(Queue<T> queue);
+    List<String> generateBinaryNumbers(int n);
+    void hospitalTriageSystem();
+    void StackUsingQueues();
+    void CircularBuffer();
+}
+
