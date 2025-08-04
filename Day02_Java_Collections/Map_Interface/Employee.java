73e01bc2647eeab15767a2c6fc6d08549a3a9d10
aarshabh8171 <saubharia@gmail.com>
2025-08-04 11:56:24 +0530

    [Aarshabh Saubhari] ADD. Map_Interface --> Employee

---
+++ Day02_Java_Collections/Map_Interface/Employee.java
@@ -0,0 +1,16 @@
+package Day02_Java_Collections.Map_Interface;
+
+public class Employee {
+    String name;
+    String department;
+
+    public Employee(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
