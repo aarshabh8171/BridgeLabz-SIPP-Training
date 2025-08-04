b238d3b53471c39537a51fd67ed9ebcd448d965b
aarshabh8171 <saubharia@gmail.com>
2025-08-04 11:13:08 +0530

    [Aarshabh Saubhari] ADD. AI_Driven_Resume_Screening_System --> DataScientist

---
+++ Day01_Java_Generics/AI_Driven_Resume_Screening_System/DataScientist.java
@@ -0,0 +1,9 @@
+package Day01_Java_Generics.AI_Driven_Resume_Screening_System;
+
+public class DataScientist extends JobRole {
+
+    @Override
+    void showRole() {
+        System.out.println("Data Scientist");
+    }
+}
