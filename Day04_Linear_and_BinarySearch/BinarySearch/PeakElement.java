9713713354b6b78e22e18a0f6859f0ff563fbb9c
aarshabh8171 <saubharia@gmail.com>
2025-08-04 10:22:42 +0530

    [Aarshabh Saubhari] ADD. BinarySearch --> PeakElement

---
+++ Day04_Linear_and_BinarySearch/BinarySearch/PeakElement.java
@@ -0,0 +1,20 @@
+// Binary Search - Find Peak Element
+package Day04_Linear_and_BinarySearch.BinarySearch;
+public class PeakElement {
+    public static int findPeak(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < arr[mid + 1])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return arr[left];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 20, 4, 1, 0};
+        System.out.println("Peak element: " + findPeak(nums));
+    }
+}
