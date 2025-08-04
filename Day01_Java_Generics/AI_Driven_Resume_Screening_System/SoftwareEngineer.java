429103e88ea03c440a497199bfe7672611340c93
aarshabh8171 <saubharia@gmail.com>
2025-08-04 11:15:15 +0530

    [Aarshabh Saubhari] ADD. AI_Driven_Resume_Screening_System --> SoftwareEngineer

---
+++ Day01_Java_Generics/AI_Driven_Resume_Screening_System/SoftwareEngineer.java
@@ -0,0 +1,9 @@
+package Day01_Java_Generics.AI_Driven_Resume_Screening_System;
+
+public class SoftwareEngineer extends JobRole{
+
+    @Override
+    void showRole() {
+        System.out.println("Software Engineer");
+    }
+}
