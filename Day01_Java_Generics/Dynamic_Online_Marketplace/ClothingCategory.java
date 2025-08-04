b1b3509053e6ab035541f1234539bf0c9c92135a
aarshabh8171 <saubharia@gmail.com>
2025-08-04 11:23:19 +0530

    [Aarshabh Saubhari] ADD. Dynamic_Online_Marketplace --> ClothingCategory

---
+++ Day01_Java_Generics/Dynamic_Online_Marketplace/ClothingCategory.java
@@ -0,0 +1,8 @@
+package Day01_Java_Generics.Dynamic_Online_Marketplace;
+
+class ClothingCategory implements Category {
+    @Override
+    public String getCategoryName() {
+        return "Clothing";
+    }
+}
