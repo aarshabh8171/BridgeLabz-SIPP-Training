8f26d03113155b4b599f96dff43869b459e18670
aarshabh8171 <saubharia@gmail.com>
2025-08-04 11:22:10 +0530

    [Aarshabh Saubhari] ADD. Dynamic_Online_Marketplace --> BookCategory

---
+++ Day01_Java_Generics/Dynamic_Online_Marketplace/BookCategory.java
@@ -0,0 +1,8 @@
+package Day01_Java_Generics.Dynamic_Online_Marketplace;
+
+class BookCategory implements Category {
+    @Override
+    public String getCategoryName() {
+        return "Books";
+    }
+}
