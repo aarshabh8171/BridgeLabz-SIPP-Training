22d929008dd5f7959c30393760d0605baa0b53d2
aarshabh8171 <saubharia@gmail.com>
2025-08-04 11:14:01 +0530

    [Aarshabh Saubhari] ADD. AI_Driven_Resume_Screening_System --> ProductManager

---
+++ Day01_Java_Generics/AI_Driven_Resume_Screening_System/ProductManager.java
@@ -0,0 +1,10 @@
+package Day01_Java_Generics.AI_Driven_Resume_Screening_System;
+
+public class ProductManager extends JobRole {
+
+    @Override
+    void showRole() {
+        System.out.println("Product Manager");
+    }
+
+}
