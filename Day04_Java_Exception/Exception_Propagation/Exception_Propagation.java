df4c10c086bc98b6405bb3e56bb02f9251811470
aarshabh8171 <saubharia@gmail.com>
2025-08-25 15:09:04 +0530

    [Aarshabh Saubhari] ADD. Java_Generics_Collection_Exception ---> Exception_Propagation

---
+++ Day04_Java_Exception/Exception_Propagation/Exception_Propagation.java
@@ -0,0 +1,19 @@
+package Day04_Java_Exception.Exception_Propagation;
+
+public class Exception_Propagation {
+    static void method1() {
+        System.out.println(10 / 0);
+    }
+
+    static void method2() {
+        method1();
+    }
+
+    public static void main(String[] args) {
+        try {
+            method2();
+        } catch (ArithmeticException e) {
+            System.out.println("Handled exception in main");
+        }
+    }
+}
