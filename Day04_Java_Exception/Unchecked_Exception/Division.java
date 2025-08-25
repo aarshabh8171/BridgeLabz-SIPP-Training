cd9a19b932bc0160619cb19cf01272074006a98c
aarshabh8171 <saubharia@gmail.com>
2025-08-25 15:13:01 +0530

    [Aarshabh Saubhari] ADD. Java_Generics_Collection_Exception ---> Unchecked_exception

---
+++ Day04_Java_Exception/Unchecked_Exception/Division.java
@@ -0,0 +1,20 @@
+package Day04_Java_Exception.Unchecked_Exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Division {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Enter number 1: "); int num1 = sc.nextInt();
+            System.out.print("Enter number 2: "); int num2 = sc.nextInt();
+            System.out.println("Result: " + (num1 / num2));
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input");
+        }
+        sc.close();
+    }
+}
