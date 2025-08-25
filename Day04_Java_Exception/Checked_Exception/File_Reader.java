eb54fbb1ba309c392db7c13b38f0f76b570f2d30
aarshabh8171 <saubharia@gmail.com>
2025-08-25 15:06:12 +0530

    [Aarshabh Saubhari] ADD. Java_Generics_Collection ---> File_Reader

---
+++ Day04_Java_Exception/Checked_Exception/File_Reader.java
@@ -0,0 +1,21 @@
+package Day04_Java_Exception.Checked_Exception;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class File_Reader {
+    public static void main(String[] args) {
+        try 
+        {
+            BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+             String line;
+            while ((line = br.readLine()) != null) System.out.println(line);
+            br.close();
+        } 
+        catch (IOException e) 
+        {
+             System.out.println("File not found");
+        }
+    }
+}
